b63c8fb043bfcd74a2181ec08c557600b9c6eb7b
mac <nan8278@126.com>
2026-04-27 21:09:47 +0800

    fix: restore adsterra script handling

--- frontend/src/components/adsterra-banner.tsx
+++ frontend/src/components/adsterra-banner.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 type AdsterraBannerProps = {
   adKey: string;
   width: number;
@@ -7,18 +9,20 @@ type AdsterraBannerProps = {
 export default function AdsterraBanner({ adKey, width, height }: AdsterraBannerProps) {
   return (
     <div style={{ width, height, overflow: "hidden" }}>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `atOptions = {
+      <Script id={`adsterra-${adKey}-options`} strategy="afterInteractive">
+        {`window.atOptions = {
   key: '${adKey}',
   format: 'iframe',
   height: ${height},
   width: ${width},
   params: {}
-};`,
-        }}
+};`}
+      </Script>
+      <Script
+        id={`adsterra-${adKey}-invoke`}
+        strategy="afterInteractive"
+        src={`https://www.highperformanceformat.com/${adKey}/invoke.js`}
       />
-      <script src={`https://www.highperformanceformat.com/${adKey}/invoke.js`} />
     </div>
   );
 }
